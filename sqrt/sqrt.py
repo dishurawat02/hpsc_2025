0f2c8909c1fcb50cd824b1b1a4365157956bb6e5
dishurawat02 <b22297@students.iitmandi.ac.in>
2025-02-02 10:32:56 +0000

    Added more efficient way of calculating square root with user input

--- sqrt/sqrt.py
+++ sqrt/sqrt.py
@@ -1,6 +1,12 @@
 s = 1
-x = 2
-
-for i in range (10):
+x = int(input("Enter the number: "))
+ea = 0.05
+es = 100
+i = 1
+while ea<es:
+	sold = s
 	s =(s + x/s)*0.5
+	es = abs((sold - s)/sold)
+	i = i+1
+print("Number of iteration is ",i)
 print(s)
